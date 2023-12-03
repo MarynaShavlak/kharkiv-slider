b53d3df30b4d60dffde2f969c2c540e4aaa20494
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-12-03 20:39:08 +0200

    add controls

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   let currentSlide = 0;
+  let currentThumbnailSlide = 0;
 
   function showSlide(index) {
     const slider = $('.slider');
@@ -14,7 +15,7 @@ $(document).ready(function(){
     currentSlide = index;
     
     const transformValue = -index * slideWidth;
-    slider.css('transform', `translateX(${transformValue}px)`);
+    slider.css('transform', `translateX(${transformValue}px)`); 
   }
 
   function nextSlide() {
@@ -27,11 +28,46 @@ $(document).ready(function(){
 
   function goToSlide(index) {
     showSlide(index);
+    showThumbnailSlide(index);
+  }
+
+
+  function showThumbnailSlide(index) {
+    console.log('click');
+    const thumbnailSlider = $('.thumbnails-slider');
+    console.log('thumbnailSlider: ', thumbnailSlider);
+    const thumbnailSlideWidth = $('.thumbnail').width();
+    console.log('thumbnailSlideWidth: ', thumbnailSlideWidth);
+
+    if (index < 0) {
+      index = thumbnailSlider.children().length - 1;
+    } else if (index >= thumbnailSlider.children().length) {
+      index = 0;
+    }
+
+    currentThumbnailSlide = index;
+
+    const transformValue = -index * thumbnailSlideWidth;
+    thumbnailSlider.css('transform', `translateX(${transformValue}px)`);
+  }
+
+  function nextThumbnailSlide() {
+    showThumbnailSlide(currentThumbnailSlide + 1);
+  }
+
+  function prevThumbnailSlide() {
+    showThumbnailSlide(currentThumbnailSlide - 1);
   }
 
 
   $('.control-btn.next').on('click', nextSlide);
   $('.control-btn.prev').on('click', prevSlide);
+  $('.thumbnail').on('click', function () {
+    const index = $(this).index();
+    goToSlide(index);
+  });
+  $('.thumbnail-control-btn.next').on('click', nextThumbnailSlide);
+  $('.thumbnail-control-btn.prev').on('click', prevThumbnailSlide);
   $('.thumbnail').on('click', function () {
     const index = $(this).index();
     goToSlide(index);
