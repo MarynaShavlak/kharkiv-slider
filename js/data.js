16d8ab608a3e9215c917e490cf3b7ce3262b83f1
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-25 14:53:21 +0300

    add new infor about places

--- js/data.js
+++ js/data.js
@@ -1,25 +1,39 @@
 export const dataObject = {
     title: 'Welcome to Kharkiv!!!',
     images: [
-        { src: '1.jpg', name: 'Freedom Square', link: 'https://maps.google.com?q=Freedom+Square+Kharkiv', address: 'Kharkiv, Freedom Square' },
-        { src: '2.jpg', name: 'Gorky Central Park', link: 'https://maps.google.com?q=Gorky+Central+Park+Kharkiv', address: 'Kharkiv, Gorky Central Park' },
+        { src: '1.jpg', name: 'Freedom Square', link: 'https://maps.app.goo.gl/AiCXSG6EbTeZdM5M8', address: 'Kharkiv, Freedom Square' },
+        { src: '2.jpg', name: 'Kharkiv Opera House', link: 'https://maps.google.com?q=Kharkiv+Opera+House', address: 'Sumska St, 25' },
+        { src: '3.jpg', name: 'Blahovishchensʹkyy Kafedralʹnyy Sobor', link: 'https://maps.app.goo.gl/Gy9iebVZJUyoF5Tn9', address: 'Blahovishchenska Square, 1' },
+        { src: '4.jpg', name: 'Freedom Monument', link: 'https://maps.google.com?q=Freedom+Monument+Kharkiv', address: 'Kharkiv, Konstytutsii Square' },
+        { src: '5.jpg', name: 'Skver Strilka', link: 'https://maps.app.goo.gl/x2auQYhypFHM6yZEA', address: 'Bannyi Ln, 2' },
+        { src: '6.jpg', name: 'Pivdennyy Vokzal Kharkiv', link: 'https://maps.app.goo.gl/hxAasiSPJL9WKQv27', address: 'Pryvokzal\'na Square, 1' },
+        { src: '7.jpg', name: 'Assumption Cathedral', link: 'https://maps.app.goo.gl/xJvsmotuaei7okX49', address: 'Universytets\'ka St, 11' },
+        { src: '8.jpg', name: 'Central Park', link: 'https://maps.app.goo.gl/vdZDRu9HiSDi4LHS8', address: 'Sumska St, 81' },
+
+
+
+
+
         { src: '3.jpg', name: 'Shevchenko Park', link: 'https://maps.google.com?q=Shevchenko+Park+Kharkiv', address: 'Kharkiv, Shevchenko Park' },
         { src: '4.jpg', name: 'Kharkiv National University', link: 'https://maps.google.com?q=Kharkiv+National+University', address: 'Kharkiv, Kharkiv National University' },
         { src: '5.jpg', name: 'Kharkiv Zoo', link: 'https://maps.google.com?q=Kharkiv+Zoo', address: 'Kharkiv, Kharkiv Zoo' },
         { src: '6.jpg', name: 'Pokrovsky Monastery', link: 'https://maps.google.com?q=Pokrovsky+Monastery+Kharkiv', address: 'Kharkiv, Pokrovsky Monastery' },
-        { src: '7.jpg', name: 'Kharkiv Cathedral', link: 'https://maps.google.com?q=Kharkiv+Cathedral', address: 'Kharkiv, Kharkiv Cathedral' },
+
+
+
+
         { src: '8.jpg', name: 'Mirror Stream', link: 'https://maps.google.com?q=Mirror+Stream+Kharkiv', address: 'Kharkiv, Mirror Stream' },
         { src: '9.jpg', name: 'Ukrainian State Railway Museum', link: 'https://maps.google.com?q=Ukrainian+State+Railway+Museum+Kharkiv', address: 'Kharkiv, Ukrainian State Railway Museum' },
         { src: '10.jpg', name: 'Vernadsky Boulevard', link: 'https://maps.google.com?q=Vernadsky+Boulevard+Kharkiv', address: 'Kharkiv, Vernadsky Boulevard' },
         { src: '11.jpg', name: 'Metalist Stadium', link: 'https://maps.google.com?q=Metalist+Stadium+Kharkiv', address: 'Kharkiv, Metalist Stadium' },
         { src: '12.jpg', name: 'Annunciation Cathedral', link: 'https://maps.google.com?q=Annunciation+Cathedral+Kharkiv', address: 'Kharkiv, Annunciation Cathedral' },
-        { src: '13.jpg', name: 'Kharkiv Opera House', link: 'https://maps.google.com?q=Kharkiv+Opera+House', address: 'Kharkiv, Kharkiv Opera House' },
+
         { src: '14.jpg', name: 'Sumska Street', link: 'https://maps.google.com?q=Sumska+Street+Kharkiv', address: 'Kharkiv, Sumska Street' },
-        { src: '15.jpg', name: 'Freedom Monument', link: 'https://maps.google.com?q=Freedom+Monument+Kharkiv', address: 'Kharkiv, Freedom Monument' },
-        { src: '16.jpg', name: 'Trinity Hill', link: 'https://maps.google.com?q=Trinity+Hill+Kharkiv', address: 'Kharkiv, Trinity Hill' },
+
+
         { src: '17.jpg', name: 'Kharkiv Circus', link: 'https://maps.google.com?q=Kharkiv+Circus', address: 'Kharkiv, Kharkiv Circus' },
         { src: '18.jpg', name: 'Derzhprom', link: 'https://maps.google.com?q=Derzhprom+Kharkiv', address: 'Kharkiv, Derzhprom' },
         { src: '19.jpg', name: 'Skver Shevchenko', link: 'https://maps.google.com?q=Skver+Shevchenko+Kharkiv', address: 'Kharkiv, Skver Shevchenko' },
-        { src: '20.jpg', name: 'Cherepanov Museum', link: 'https://maps.google.com?q=Cherepanov+Museum+Kharkiv', address: 'Kharkiv, Cherepanov Museum' },
+
     ],
 };
